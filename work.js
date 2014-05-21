aa78271f8a921fc8540f862d09618c4ad6b4a3cd
Dave Tapley <dukedave@gmail.com>
2014-05-21 13:52:01 -0700

    Remove unneeded functionality

--- work.js
+++ work.js
@@ -5,25 +5,10 @@ pg = require('pg')["native"];
 pgConString = process.env.DATABASE_URL;
 
 doWork = function() {
-  var startTime;
-  startTime = process.hrtime();
-  return pg.connect(pgConString, function(err, client, done) {
-    if (process.env.SELECT1) {
-      return client.query("SELECT 1", [], function(err, result) {
-        console.log('Done in ' + process.hrtime(startTime));
-        return done(client);
-      });
-    } else if (process.env.USE1) {
-      return client.query("SELECT 1 AS one", [], function(err, result) {
-        var one;
-        one = result.rows[0].one;
-        console.log('Done in ' + process.hrtime(startTime) + ' ' + one);
-        return done(client);
-      });
-    } else {
-      console.log('Done in ' + process.hrtime(startTime));
-      return done(client);
-    }
+  var startTime = process.hrtime();
+  pg.connect(pgConString, function(err, client, done) {
+    console.log('Done in ' + process.hrtime(startTime));
+    done(client);
   });
 };
 
